528493a29b7f26ad3bcefcb80a231780a081cde2
kerwuin <kerwuin1992@outlook.com>
2023-05-09 09:18:21 -0400

    Ejercicio parte 1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
+/* Ejercicio
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,32 +12,56 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}  */
 
+
+// Ejercisio parte 1
+const App = () => {  
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  //Funsiones flecha 
   const Header = ({course}) => <h1>{course}</h1>;
-  const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => <p>{part1}{part2}{part3}{exercises1}{exercises2}{exercises3}</p>;
-  const Total = ({exercises1, exercise2, exercise3}) => <p>{exercises1} {exercise2} {exercise3}</p>;
+  const Content = ({part, exercises}) => <p>{part} {exercises}</p>;
+  const Total = ({children, exercises}) => <p>{children} {exercises}</p>;
 
-
+  /* Otra manera de crear una funcion con props: 
+  const Header = (props) => {
+    return <h1>{props.course}</h1>
+  }  */
 
   return (
     <>
       <Header course={course} />
-      <p>
-        <Content part1={part1} exercises1={exercises1} />
-      </p>
-      <p>
-        <Content part2={part2} exercises2={exercises2} />
-      </p>
-      <p>
-        <Content part3={part3} exercises3={exercises3} />
-      </p>
-      <p>        
-        <Total Number of exercises exercises1={exercises1 + exercises2 + exercises3} />
-      </p>
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total children={"Number of exercises"} exercises={exercises1 + exercises2 + exercises3} />
     </>
   )
 }
 
 
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'))
