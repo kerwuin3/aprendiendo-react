e1ed36d043de8ef4c55722c9c843ff3051c9b1c1
Kerwuin Chirino <120334287+kerwuin3@users.noreply.github.com>
2023-05-09 13:19:10 -0400

    Update index.js

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -56,7 +56,7 @@ const App = () => {
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
-      <Total children={"Number of exercises"} exercises={exercises1 + exercises2 + exercises3} />
+      <Total children={'Number of exercises'} exercises={exercises1 + exercises2 + exercises3} />
     </>
   )
 }
